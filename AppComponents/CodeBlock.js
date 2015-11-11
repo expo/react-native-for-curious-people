3adb07b220603ee50ef5be21e5e311990265dd05
Brent Vatne <brentvatne@gmail.com>
2015-11-11 19:49:58 +0100

    Improve CodeBlock a bit more

--- AppComponents/CodeBlock.js
+++ AppComponents/CodeBlock.js
@@ -6,8 +6,6 @@
 'use strict';
 
 import React, {
-  AppRegistry,
-  StyleSheet,
   Text,
   View,
   ScrollView,
@@ -16,12 +14,6 @@ import React, {
 import Prism from 'prism';
 
 export default class CodeBlock extends React.Component {
-  constructor(props, context) {
-    super(props, context);
-    this.state = {
-    };
-  }
-
   render() {
     let result = Prism.highlight(
       this.props.code,
@@ -30,14 +22,11 @@ export default class CodeBlock extends React.Component {
     );
 
     return (
-      <View style={{marginTop: this.props.marginTop, marginBottom: this.props.marginBottom || 15, backgroundColor: 'rgba(0,0,0,0.02)'}}>
-        <ScrollView
-          horizontal
-          bounces={false}
-          contentContainerStyle={{paddingHorizontal: 10}}>
-            <Text style={{fontSize: 13,}}>
-              {result}
-            </Text>
+      <View style={{marginTop: this.props.marginTop || -2, marginBottom: this.props.marginBottom || 10, backgroundColor: 'rgba(0,0,0,0.01)', borderRadius: 3}}>
+        <ScrollView horizontal bounces={false} contentContainerStyle={{paddingHorizontal: 8}}>
+          <Text style={{fontSize: 13,}}>
+            {result}
+          </Text>
         </ScrollView>
       </View>
     );
